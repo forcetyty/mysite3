415f49e2ffda4ff1e16f0f636479907177c90615
forcetyty <forcetyty@gmail.com>
2019-09-27 10:10:42 +0900

    불필요한

--- src/main/java/kr/co/itcen/mysite/controller/BoardController.java
+++ src/main/java/kr/co/itcen/mysite/controller/BoardController.java
@@ -1,16 +1,12 @@
 package kr.co.itcen.mysite.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
-import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
